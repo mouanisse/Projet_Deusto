839d223f800f31aaab24699ddb906200467c6de0
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-06-10 11:42:20 +0200

    Update Mammo_utils.py

--- Mammo_utils.py
+++ Mammo_utils.py
@@ -7,6 +7,7 @@ from PIL import Image
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 
 
