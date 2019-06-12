6a4d344b62ef5d9c85975336ffe56d7ea4e6c445
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-06-12 14:58:06 +0200

    Update Mammo_utils.py

--- Mammo_utils.py
+++ Mammo_utils.py
@@ -117,7 +117,7 @@ class Mammographie:
     def train(self):
 
         # Define epoch and batch_size
-        epochs = 15
+        epochs = 25
         batch_size = 100
 
         # First, we need to prepare our training and testing data, and pre-process it
