39f1dc61b1ee64586d5ae016a3289d280931eca7
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-06-13 09:40:50 +0200

    Update Mammo_utils.py

--- Mammo_utils.py
+++ Mammo_utils.py
@@ -105,10 +105,6 @@ class Mammographie:
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))
         
-        model.add(keras.layers.Conv2D(320, (3, 3), activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.MaxPooling2D(2, 2))
-        
         model.add(keras.layers.Conv2D(1024, (3, 3), activation='relu'))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))
