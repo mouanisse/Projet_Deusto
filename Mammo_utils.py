a727f61eec64a57fe9b480e6d75c68550deed10b
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-06-14 13:50:00 +0200

    Update Mammo_utils.py

--- Mammo_utils.py
+++ Mammo_utils.py
@@ -32,39 +32,91 @@ class Mammographie:
 
 
     def database_preprocessing(self):
-        tumor = np.ones((self.database_size,1))
-        npy_dataset = np.empty([self.database_size, self.height, self.width])
+        train_data = []
+        val_data = []
+        test_data = []
+        train_labels = []
+        val_labels = []
+        test_labels = []
+
+
         for root, dirs, files in os.walk(self.database_path + '/', topdown=False):
             # Loop through files
+
+            train_iteration = math.floor(0.8 * len(files))
+            val_iteration = math.floor(0.1 * len(files))
+            test_iteration = len(files) - (math.floor(0.8 * len(files)) + math.floor(0.1 * len(files)))
+
+
             if root[-1:] == '0':  # normal images
-                k = 0
-                for f in files:
-                    if f != '.DS_Store' and f != '.DS_Stoe**':
-                        tumor[k] = 0
-                        npy_dataset[k][:][:][:] = np.array(Image.open(root + '/' + f))
-                        k = k + 1
+
+                for i in range(train_iteration):
+
+                    if files[i] != '.DS_Store' and files[i] != '.DS_Stoe**':
+                        train_labels.append(0)
+                        train_data.append(np.array(Image.open(root + '/' + files[i])))
+
+                for j in range(train_iteration, train_iteration + val_iteration):
+
+                    if files[j] != '.DS_Store' and files[j] != '.DS_Stoe**':
+                        val_labels.append(0)
+                        val_data.append(np.array(Image.open(root + '/' + files[j])))
+
+                for k in range(train_iteration + val_iteration,
+                                train_iteration + val_iteration + test_iteration):
+
+                    if files[k] != '.DS_Store' and files[k] != '.DS_Stoe**':
+                        test_labels.append(0)
+                        test_data.append(np.array(Image.open(root + '/' + files[k])))
+
 
 
             elif root[-1:] == '8':  # anormal images
-                k = 0
-                for f in files:
-                    if f != '.DS_Store' and f != '.DS_Stoe**':
-                        npy_dataset[k][:][:][:] = np.array(Image.open(root + '/' + f))
-                        k = k + 1
 
-        # Transform the labels to use categorical_crossentropy (more than 2 classes) as a loss function
-        labels = tumor
+                for i in range(train_iteration):
 
-        # Split into training and testing sets
-        train_data, test_val_data, train_labels, test_val_labels = train_test_split(npy_dataset, labels, test_size=0.2,
-                                                                                    random_state=42)
-        test_data, val_data, test_labels, val_labels = train_test_split(test_val_data, test_val_labels, test_size=0.5,
-                                                                                    random_state=42)
+                    if files[i] != '.DS_Store' and files[i] != '.DS_Stoe**':
+                        train_labels.append(1)
+                        train_data.append(np.array(Image.open(root + '/' + files[i])))
+
+                for j in range(train_iteration, train_iteration + val_iteration):
+
+                    if files[j] != '.DS_Store' and files[j] != '.DS_Stoe**':
+                        val_labels.append(1)
+                        val_data.append(np.array(Image.open(root + '/' + files[j])))
+
+                for k in range(train_iteration + val_iteration,
+                               train_iteration + val_iteration + test_iteration):
+
+                    if files[k] != '.DS_Store' and files[k] != '.DS_Stoe**':
+                        test_labels.append(1)
+                        test_data.append(np.array(Image.open(root + '/' + files[k])))
+
+
+        # Convert lists into arrays
+        train_labels = np.asarray(train_labels)
+        val_labels = np.asarray(val_labels)
+        test_labels = np.asarray(test_labels)
+        train_data = np.asarray(train_data)
+        val_data = np.asarray(val_data)
+        test_data = np.asarray(test_data)
+
+
+        # Shuffle the data and the labels simultanousely
+        train_data, train_labels = shuffle(train_data, train_labels)
+        val_data, val_labels = shuffle(val_data, val_labels)
+        test_data, test_labels = shuffle(test_data, test_labels)
+
+
 
         # To get shape (129, 129, 1)
         train_data = np.expand_dims(train_data, 3)
         val_data = np.expand_dims(val_data, 3)
         test_data = np.expand_dims(test_data, 3)
+        train_labels = np.expand_dims(train_labels, -1)
+        val_labels = np.expand_dims(val_labels, -1)
+        test_labels = np.expand_dims(test_labels, -1)
+
 
         # We fill the variables defined by 'None', with their respective values
         self.train_data = train_data
@@ -73,11 +125,33 @@ class Mammographie:
         self.train_labels = train_labels
         self.val_labels = val_labels
         self.test_labels = test_labels
-        
-        print(labels.max())
-        print(labels.min())
-        print(train_data[0].max())
-        print(train_data[0].min())
+
+
+
+        print(train_labels.max())
+        print(train_labels.min())
+        print(train_data.max())
+        print(train_data.min())
+
+        print('*********************************')
+
+        print(train_data.shape)
+        print(val_data.shape)
+        print(test_data.shape)
+        print(train_labels.shape)
+        print(val_labels.shape)
+        print(test_labels.shape)
+
+
+
+        np.save('Projet_Deusto/Numpy_dataset/train_data_('+str(self.height)+','+str(self.width)+')_2112.npy', train_data)
+        np.save('Projet_Deusto/Numpy_dataset/val_data_(' + str(self.height) + ',' + str(self.width) + ')_2112.npy', val_data)
+        np.save('Projet_Deusto/Numpy_dataset/test_data_('+str(self.height)+','+str(self.width)+')_2112.npy', test_data)
+
+
+        np.save('Projet_Deusto/Numpy_dataset/train_labels_('+str(self.height)+','+str(self.width)+')_2112.npy', train_labels)
+        np.save('Projet_Deusto/Numpy_dataset/val_labels_(' + str(self.height) + ',' + str(self.width) + ')_2112.npy', val_labels)
+        np.save('Projet_Deusto/Numpy_dataset/test_labels_('+str(self.height)+','+str(self.width)+')_2112.npy', test_labels)
 
 
 
@@ -92,15 +166,15 @@ class Mammographie:
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))  # divise par 4 les dimensions de l'image
 
-        model.add(keras.layers.Conv2D(80, (3, 3), activation='relu'))
+        model.add(keras.layers.Conv2D(80, (4, 4), activation='relu'))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))  # divise par 4 les dimensions de l'image
 
-        model.add(keras.layers.Conv2D(160, (2, 2), activation='relu'))#, activity_regularizer=keras.regularizers.l1(0.001)))
+        model.add(keras.layers.Conv2D(160, (3, 3), activation='relu'))#, activity_regularizer=keras.regularizers.l1(0.001)))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))
         
-        model.add(keras.layers.Conv2D(320, (2, 2), activation='relu'))#, activity_regularizer=keras.regularizers.l1(0.001)))
+        model.add(keras.layers.Conv2D(320, (3, 3), activation='relu'))#, activity_regularizer=keras.regularizers.l1(0.001)))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))
 
